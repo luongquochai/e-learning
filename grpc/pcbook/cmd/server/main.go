b19b46942c6ca933c96417c86cd1302d4ecfacb6
Hai Luong Quoc <luongquochai0707@gmail.com>
2024-09-03 20:56:43 +0700

    gRPC reflection and Evans CLI

--- grpc/pcbook/cmd/server/main.go
+++ grpc/pcbook/cmd/server/main.go
@@ -9,6 +9,7 @@ import (
 	"github.com/luongquochai/e-learning/tree/main/grpc/pcbook/pb"
 	"github.com/luongquochai/e-learning/tree/main/grpc/pcbook/service"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 func main() {
@@ -24,6 +25,7 @@ func main() {
 
 	grpcServer := grpc.NewServer(serverOptions...)
 	pb.RegisterLaptopServiceServer(grpcServer, laptopServer)
+	reflection.Register(grpcServer)
 
 	address := fmt.Sprintf("0.0.0.0:%d", *port)
 	listener, err := net.Listen("tcp", address)
